2e3a7a88edbb167a921fbad4dafdb502c45e5b95
jasondavis4422 <jason.a.davis.25@dartmouth.edu>
2022-11-29 11:04:37 -0600

    Finished intro. Organized and added trials & endpage.

--- drafts/ctom.js
+++ drafts/ctom.js
@@ -138,5 +138,30 @@ function nextPage (page_number)
     
 
 }
-// 5 Pages: Introduction (0), Consent Page (1), Fill-In(2), Trials(3), End Page(4)
-// 5 Trials: generate the stimulus set before beginning the trials, save the trial
+//clock counter, if necessary
+function currentTime() {
+    let date = new Date(); 
+    let hh = date.getHours();
+    let mm = date.getMinutes();
+    let ss = date.getSeconds();
+    let session = "AM";
+  
+    if(hh == 0){
+        hh = 12;
+    }
+    if(hh > 12){
+        hh = hh - 12;
+        session = "PM";
+     }
+  
+     hh = (hh < 10) ? "0" + hh : hh;
+     mm = (mm < 10) ? "0" + mm : mm;
+     ss = (ss < 10) ? "0" + ss : ss;
+      
+     let time = hh + ":" + mm + ":" + ss + " " + session;
+  
+    document.getElementById("clock").innerText = time; 
+    let t = setTimeout(function(){ currentTime() }, 1000);
+  }
+  currentTime();
+//<div id = "clock" onload="currentTime()"></div>// (HTML Code)
