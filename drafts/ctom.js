2142b61d82459b91eea00e3de4936adfbc9f014d
jasondavis4422 <jason.a.davis.25@dartmouth.edu>
2022-11-30 12:13:13 -0600

    Same commit as others.

--- drafts/ctom.js
+++ drafts/ctom.js
@@ -18,10 +18,8 @@
 //
 // Participant Information - need to make ParticipantSet an array of Participants
 const participant_set = [];
-class Participant
-{
-    constructor()
-    {
+class Participant {
+    constructor() {
         this.name = "";
         this.ID = "";
         this.age = 0;
@@ -32,36 +30,32 @@ participant_set.push(subject);
 
 // Stimulus Set Information - need to make StimulusSet an array of Stimuluses
 const stimulus_set = [];
-class Stimulus
-{
-constructor(pair_id)
-{
-    this.num_of_pairs = 5;
-    this.num_of_trials = 5;
-    this.num_of_images = 30;
-    this.num_of_videos = 30;
-    this.pair_num = pair_id;
-    this.trial_arr = [];
-    this.image_arr = [];
-    this.video_arr = [];
+class Stimulus {
+    constructor(pair_id) {
+        this.num_of_pairs = 5;
+        this.num_of_trials = 5;
+        this.num_of_images = 30;
+        this.num_of_videos = 30;
+        this.pair_num = pair_id;
+        this.trial_arr = [];
+        this.image_arr = [];
+        this.video_arr = [];
 
-}
-randomGenerate()
-{
-    for (var trial_number = 0; trial_number < this.num_of_trials; trial_number++)
-    {
-        var image_set = Math.floor(Math.random() * this.num_of_images);
-        var video_set = Math.floor(Math.random() * this.num_of_videos);
-        console.log("Image:" + image_set);
-        console.log("Video:" + video_set);
-        console.log(" ");
-        this.image_arr[trial_number] = image_set;
-        this.video_arr[trial_number] = video_set;
-        this.trial_arr[trial_number] = trial_number;
+    }
+    randomGenerate() {
+        for (var trial_number = 0; trial_number < this.num_of_trials; trial_number++) {
+            var image_set = Math.floor(Math.random() * this.num_of_images);
+            var video_set = Math.floor(Math.random() * this.num_of_videos);
+            console.log("Image:" + image_set);
+            console.log("Video:" + video_set);
+            console.log(" ");
+            this.image_arr[trial_number] = image_set;
+            this.video_arr[trial_number] = video_set;
+            this.trial_arr[trial_number] = trial_number;
+        }
     }
 }
-}
-const Pair = new Stimulus (1);
+const Pair = new Stimulus(1);
 Pair.randomGenerate();
 stimulus_set.push(Pair);
 console.log(Pair.image_arr);
@@ -69,99 +63,84 @@ console.log(Pair.image_arr);
 
 //Data Set Information - make Collective_Data an array of Indiv_Data
 const data_set = [];
-class Individual_Data
-{
-constructor(person)
-{
-    this.personnum = person;
-    this.img_ratings = [];
-    this.vid_ratings = [];
-    this.collab_vid_ratings = [];
-    this.pair_id = 0;
-    if (person == 1)
-    {
-        this.pair_id = 1;
+class Individual_Data {
+    constructor(person) {
+        this.personnum = person;
+        this.img_ratings = [];
+        this.vid_ratings = [];
+        this.collab_vid_ratings = [];
+        this.pair_id = 0;
+        if (person == 1) {
+            this.pair_id = 1;
+        }
+        if (person % 2 != 0) {
+            this.pair_id = (person / 2) + 0.5;
+        }
+        else {
+            this.pair_id = person / 2;
+        }
     }
-    if (person % 2 != 0)
-    {
-        this.pair_id = (person/2) + 0.5;
+    addNewRating(input, which_dataset) {
+        if (which_dataset == 1) {
+            this.img_ratings.push(input);
+        }
+        if (which_dataset == 2) {
+            this.vid_ratings.push(input);
+        }
+        else {
+            this.collab_vid_ratings.push(input);
+        }
     }
-    else
-    {
-        this.pair_id = person/2;
-    }
-}
-addNewRating(input, which_dataset)
-{
-   if (which_dataset == 1)
-   {
-    this.img_ratings.push(input);
-   }
-   if (which_dataset == 2)
-   {
-    this.vid_ratings.push(input);
-   }
-   else
-   {
-    this.collab_vid_ratings.push(input);
-   }
-}
 }
 // Slider Information
 // Need to add a checkifUsed function to determine whether the slider has been used and return the corresponding information.
 // I think this is already part of the rangeSlider class as seen in my HTML, but I am not actually sure.
-class rangeSlider 
-{
-    constructor ()
-    {
+class rangeSlider {
+    constructor() {
         this.defaultvalue = 1;
         this.used = false;
     }
-    updateValue (input)
-    {
+    updateValue(input) {
         this.defaultvalue = input;
         this.used = true;
     }
-    returnValue ()
-    {
+    returnValue() {
         return this.defaultvalue;
     }
 
 }
 var page_number = 1;
-function nextPage (page_number)
-{
-    if (page_number < 4)
-    {
+function nextPage(page_number) {
+    if (page_number < 4) {
         page_number += 1;
     }
-    
+
 
 }
 //clock counter, if necessary
 function currentTime() {
-    let date = new Date(); 
+    let date = new Date();
     let hh = date.getHours();
     let mm = date.getMinutes();
     let ss = date.getSeconds();
     let session = "AM";
-  
-    if(hh == 0){
+
+    if (hh == 0) {
         hh = 12;
     }
-    if(hh > 12){
+    if (hh > 12) {
         hh = hh - 12;
         session = "PM";
-     }
-  
-     hh = (hh < 10) ? "0" + hh : hh;
-     mm = (mm < 10) ? "0" + mm : mm;
-     ss = (ss < 10) ? "0" + ss : ss;
-      
-     let time = hh + ":" + mm + ":" + ss + " " + session;
-  
-    document.getElementById("clock").innerText = time; 
-    let t = setTimeout(function(){ currentTime() }, 1000);
-  }
-  currentTime();
+    }
+
+    hh = (hh < 10) ? "0" + hh : hh;
+    mm = (mm < 10) ? "0" + mm : mm;
+    ss = (ss < 10) ? "0" + ss : ss;
+
+    let time = hh + ":" + mm + ":" + ss + " " + session;
+
+    document.getElementById("clock").innerText = time;
+    let t = setTimeout(function () { currentTime() }, 1000);
+}
+currentTime();
 //<div id = "clock" onload="currentTime()"></div>// (HTML Code)
