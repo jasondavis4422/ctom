beb9a46a55f2b29b27b3139f52f02720f3e376ba
jasondavis4422 <jason.a.davis.25@dartmouth.edu>
2022-12-11 18:55:44 -0600

    Added some important comments.

--- offline/accessData.js
+++ offline/accessData.js
@@ -7,9 +7,9 @@ checkBrowserorServer();
 
 var results_set = [];
 var stimulus_info = new Stimuli(5); //grab seed from local storage
-var named = "Jason Davis";
-var ID = "f005g23";
+var named = "Jason Davis"; //grab name from local storage
+var ID = "f005g23"; //grab ID from local storage
 //copy data from localStorage
 var data = '[{"img_ratings":["50","53","46","65","50"],"vid_ratings":[],"collab_vid_ratings":["50","53","46","65","50"]},{"img_ratings":["50","76","40","63","50"],"vid_ratings":[],"collab_vid_ratings":["50","76","40","63","50"]},{"img_ratings":["43","65","32","54","34"],"vid_ratings":[],"collab_vid_ratings":["43","65","32","54","34"]},{"img_ratings":["51","45","59","21","44"],"vid_ratings":[],"collab_vid_ratings":["51","45","59","21","44"]},{"img_ratings":["50","70","24","67","29"],"vid_ratings":[],"collab_vid_ratings":["50","70","24","67","29"]},{"img_ratings":["79","62","35","41","34"],"vid_ratings":[],"collab_vid_ratings":["79","62","35","41","34"]},{"img_ratings":["60","54","66","52","57"],"vid_ratings":[],"collab_vid_ratings":["60","54","66","52","57"]},{"img_ratings":["55","65","44","53","50"],"vid_ratings":[],"collab_vid_ratings":["55","65","44","53","50"]},{"img_ratings":["59","57","55","60","34"],"vid_ratings":[],"collab_vid_ratings":["59","57","55","60","34"]}]';
-const result = new Result(named, ID, data, stimulus_info.getSeedArray());
-results_set.push(result);
+const result = new Result(named, ID, data, stimulus_info.getSeedArray()); //create new Result object
+results_set.push(result); //add new object to stored offline array
